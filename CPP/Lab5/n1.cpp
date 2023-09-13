d62ebfca9a5f0136dfd5ee1336d71b0f3bd783f6
randarcanum <cerednicenco30@gmail.com>
2023-09-14 00:21:42 +0300

    dynamic+

--- CPP/Lab5/n1.cpp
+++ CPP/Lab5/n1.cpp
@@ -2,59 +2,74 @@
 #include <fstream>
 using namespace std;
 struct user {
-    char fname[256], lname[256], mname[256];
-    char age, sex, n;
-    short uses;
-    bool fl[6] = {0, 0, 0, 0, 0, 0};
-public:
+    char *lname = nullptr, *fname = nullptr, *mname = nullptr, *sex = nullptr;
+    unsigned char *age = nullptr;
+    unsigned short *uses = nullptr;
     void print() {
-        cout << "User N" << +n << ":" << endl;
-        if (fl[0]) cout << "Last name:    " << lname << endl;
-        if (fl[1]) cout << "First name:   " << fname << endl;
-        if (fl[2]) cout << "Middle name:  " << mname << endl;
-        if (fl[3]) cout << "Age:          " << +age << endl;
-        if (fl[4]) cout << "Sex:          " << sex << endl;
-        if (fl[5]) cout << "Access times: " << uses << endl;
+        if (lname) cout << "Last name:   " << lname << endl;
+        if (fname) cout << "First name:  " << fname << endl;
+        if (mname) cout << "Middle name: " << mname << endl;
+        if (age) cout << "Age:         " << *age << endl;
+        if (sex) cout << "Sex:         " << *sex << endl;
+        if (uses) cout << "Uses number: " << *uses << endl;
+    }
+    void initl(char l) {
+        lname = new char[l];
+    }
+    void initf(char l) {
+        fname = new char[l];
+    }
+    void initm(char l) {
+        mname = new char[l];
+    }
+    void release() {
+        delete[] lname;
+        delete[] fname;
+        delete[] mname;
+        delete age;
+        delete sex;
+        delete uses;
     }
 };
-char readfile(user *out) {
+char readfile(user *&out) {
     char n, m, len, blen;
     ifstream data("UserData-LR-5.bin", ios::binary);
     data.read(&n, 1);
+    out = new user[n];
     for (char i = 0; i < n; i++) {
-        data.read(&out[i].n, 1);
+        data.read(&m, 1);
         data.read(&blen, 1);
         for (char j = 0; j < 6; j++) {
+            if (blen <= 0) break;
             data.read(&m, 1);
             data.read(&len, 1);
             blen -= len+2;
             switch(m) {
                 case 1:
-                    out[i].fl[0] = 1;
+                    out[i].lname = new char[len];
                     data.read(out[i].lname, len);
                     break;
                 case 2:
-                    out[i].fl[1] = 1;
+                    out[i].fname = new char[len];
                     data.read(out[i].fname, len);
                     break;
                 case 3:
-                    out[i].fl[2] = 1;
+                    out[i].mname = new char[len];
                     data.read(out[i].mname, len);
                     break;
                 case 4:
-                    out[i].fl[3] = 1;
-                    data.read(&out[i].age, 1);
+                    out[i].age = new unsigned char;
+                    data.read((char*)out[i].age, 1);
                     break;
                 case 5:
-                    out[i].fl[4] = 1;
-                    data.read(&out[i].sex, 1);
+                    out[i].sex = new char;
+                    data.read(out[i].sex, 1);
                     break;
                 case 6:
-                    out[i].fl[5] = 1;
-                    data.read((char*)&out[i].uses, 2);
+                    out[i].uses = new unsigned short;
+                    data.read((char*)out[i].uses, 2);
                     break;
             }
-            if (blen < 1) continue;
         }
     }
     data.close();
@@ -62,23 +77,31 @@ char readfile(user *out) {
 }
 int main() {
     char ch;
-    user users[10];
-    unsigned char total = readfile(users);
+    user *users;
+    char total = readfile(users);
     while (1) {
         cin >> ch;
         switch (ch) {
             case 'a':
             case 'A':
                 for (char i = 0; i < total; i++) {
+                    cout << "User N" << i+1 << ":" << endl;
                     users[i].print();
                 }
                 break;
             case '0' ... '9':
-                if (ch-48 < total) users[ch-48].print();
+                if (ch-48 < total) {
+                    cout << "User N" << ch-47 << ":" << endl;
+                    users[ch-48].print();
+                }
                 else cout << "User N" << ch-47 << " does not exist" << endl;
                 break;
             case 'e':
             case 'E':
+                for (char i = 0; i < total; i++) {
+                    users[i].release();
+                }
+                delete[] users;
                 return 0;
         }
     }
