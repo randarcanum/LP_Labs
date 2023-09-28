c95ea6bf2326e2584e37819249e2aec3a43dd417
randarcanum <cerednicenco30@gmail.com>
2023-09-28 09:27:03 +0300

    n2 pointer sort

--- CPP/Lab1/n2.cpp
+++ CPP/Lab1/n2.cpp
@@ -8,19 +8,29 @@ int main() {
             cin >> m[i][j];
         }
     }
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 8 - i; j++) {
-            a = j / 3;
-            b = j % 3;
-            c = (j + 1) / 3;
-            d = (j + 1) % 3;
-            if (m[c][d] < m[a][b]) {
-                t = m[a][b];
-                m[a][b] = m[c][d];
-                m[c][d] = t;
+    //for (int i = 0; i < 9; i++) {
+    //    for (int j = 0; j < 8 - i; j++) {
+    //        a = j / 3;
+    //        b = j % 3;
+    //        c = (j + 1) / 3;
+    //        d = (j + 1) % 3;
+    //        if (m[c][d] < m[a][b]) {
+    //            t = m[a][b];
+    //            m[a][b] = m[c][d];
+    //            m[c][d] = t;
+    //        }
+    //    }
+    //}
+    float *klo = &m[2][2];
+    for (int i = 0; i < 9; i++, klo--) {
+        for (float *flo = m[0]; flo < klo; flo++) {
+            if (*flo > *(flo+1)) {
+                t = *flo;
+                *flo = *(flo+1);
+                *(flo+1) = t;
             }
         }
-    }   
+    }
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout << m[i][j] << " ";
